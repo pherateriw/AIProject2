a7fea92fd2eb7bd1e722d1f365afa8b29af0537f
Lisa Peters <lisapeters.peters@gmail.com>
2016-10-18 08:20:23 -0600

    RunModels.py change

--- RunModels.py
+++ RunModels.py
@@ -5,12 +5,12 @@ def main():
     print("Hello Wumpus World!")
     parser = argparse.ArgumentParser()
     parser.add_argument("size", help="Size of world", type=int)
-    parser.add_argument("o", help="Probability of obstacles", type=float)
-    parser.add_argument("p", help="Probability of pits", type=float)
-    parser.add_argument("w", help="Probability of wumpi", type=float)
+    parser.add_argument("obstacles", help="Probability of obstacles", type=float)
+    parser.add_argument("pit", help="Probability of pits", type=float)
+    parser.add_argument("wumpi", help="Probability of wumpi", type=float)
     args = parser.parse_args()
 
-    dude = Dudes.ReactiveDude(args.size, args.o, args.p, args.w)
+    dude = Dudes.ReactiveDude(args.size, args.obstacles, args.pit, args.wumpi)
 
 if __name__ == '__main__':
     main()
