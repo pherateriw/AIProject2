22a543042b4de02fd69250cfec9216614065da19
Monica Thornton <monicasuecooper@hotmail.com>
2016-10-18 10:26:47 -0600

    Added code to turn left/right 90 degrees for informed explorer. Added stub for moving informed explorer forward one step at a time. Will flesh out shortly.

--- Move.py
+++ Move.py
@@ -2,7 +2,6 @@
 # TODO add shoot arrow, wumpus scream
 # TODO right now game does not end on pit/wumpus (helps to test), fix this
 # TODO finish game/print stats if die
-# TODO figure out why "None" is printing out and fix it
 # TODO print statements in a more reasonable order?
 # TODO check score
 # TODO: arrows (in kb?)
@@ -57,8 +56,10 @@ class Move:
                 self.kb.update_cell(x, y, "_")
                 y = temp
         self.kb.update_cell(x, y, direction)
+        self.kb.update_percept(x, y)
         return x, y, self.gold_found
 
+    # places the explorer in the starting cell, facing south
     def place_dude(self):
         print("Placing Dude at (0, 0), facing south")
         self.kb.update_cell(0, 0, "v")
@@ -67,17 +68,21 @@ class Move:
     # upon move, interacts with the world (if in a relevant part of the map)
     def successful_move(self, x, y, direction):
         self.change_direction(direction)
+        # if in square with gold, grab gold
         if self.kb.unknown_map[x][y] == 'g':
             self.grab_gold()
             return True
+        # if in square with obstacle, update percept
         elif self.kb.unknown_map[x][y] == 'o':
             self.kb.update_cell(x, y, "o")
             self.kb.update_percept(x, y)
             return False
+        # if in square with pit, call pit fall handler
         elif self.kb.unknown_map[x][y] == 'p':    
             self.pit_fall()
             self.kb.update_cell(x, y, "p")
             return True
+        # if in square with wumpus, call wumpus handler
         elif self.kb.unknown_map[x][y] == 'w':    
             self.wumpus_encounter()
             self.kb.update_cell(x, y, "w")
@@ -125,6 +130,85 @@ class Move:
             self.cost -= 1
         else:
             print("Got turned around...")
+
+    # turns the explorer left 90 degrees
+    # prints out compass where location of "<", ">" ,"^" or "v" shows location explorer is facing
+    def turn_explorer_left(self):
+        self.moves += 1
+        
+        if (self.facing == "EAST"):
+            self.facing = "NORTH"
+            print(" ^ ")
+            print("W E")
+            print(" S ")             
+
+        elif (self.facing == "NORTH"):
+            self.facing = "WEST"
+            print(" N ")
+            print("< E")
+            print(" S ")         
+        
+        elif (self.facing == "WEST"):
+            self.facing = "SOUTH"            
+            print(" N ")
+            print("W E")
+            print(" v ") 
+            
+        elif (self.facing == "SOUTH"):
+            self.facing = "EAST"             
+            print(" N ")
+            print("W >")
+            print(" S ") 
+        
+        print("Explorer has turned left, is now facing {}".format(self.facing))
+
+    # turns the informed explorer right 90 degrees
+    # prints out compass where location of "<", ">" ,"^" or "v" shows location explorer is facing
+    def turn_explorer_right(self):
+        self.moves += 1
+             
+        if (self.facing == "EAST"):
+            self.facing = "SOUTH"
+            print(" N ")
+            print("W E")
+            print(" v ") 
+            
+        elif (self.facing == "NORTH"):
+            self.facing = "EAST"             
+            print(" N ")
+            print("W >")
+            print(" S ")        
+        
+        elif (self.facing == "WEST"):
+            self.facing = "NORTH"            
+            print(" ^ ")
+            print("W E")
+            print(" S ")  
+            
+        elif (self.facing == "SOUTH"):
+            self.facing = "WEST"             
+            print(" N ")
+            print("< E")
+            print(" S ")  
+        
+        print("Explorer has turned left, is now facing {}".format(self.facing))
+
+    # moves the informed explorer forward by one space    
+    def move_forward(self, x, y):
+        self.moves += 1
+        
+        if(self.facing == "EAST"):
+            #increment x by 1, y stays the same
+
+        if(self.facing == "WEST"):
+            #decrement x by 1, y stays the same                
+
+        if(self.facing == "NORTH"):
+            #x stays the same, decrement y by 1
+        
+        if(self.facing == "SOUTH"):
+            #x stays the same, increment y by 1        
+
 
 
     def grab_gold(self):
