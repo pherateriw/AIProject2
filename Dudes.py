ed88be11f85033e8eda357610662dc91362b19e2
Monica Thornton <monicasuecooper@hotmail.com>
2016-10-23 17:35:24 -0600

    Made changes to get informed dude running

--- Dudes.py
+++ Dudes.py
@@ -137,7 +137,6 @@ class InformedDude(AbstractDude):
     def __init__(self, logger, kb):
         super(InformedDude, self).__init__(logger, kb)
         self.logger.info("Informed dude created!")
-        self.logger.info()
         self.ie = InferenceEngine.InferenceEngine(kb)
         self.move.place_dude()
         self.ie.tell(['a'], self.x, self.y)
@@ -145,24 +144,140 @@ class InformedDude(AbstractDude):
 
     # while not gold found or stuck, keep moving.
     def rounds(self):
-        t = 0  # time step TODO time?
+        #t = 0  # time step TODO time?
         stop = False
+        choices = []
+        current_valid_choices = set()
+               
         
         while not stop:
-            # update percepts
-            current_percepts = self.update_percepts(self.x, self.y)
-            print(current_percepts)
+            # update the explorer's percepts for the current location
+            current_percept_key = self.kb.update_percept(self.x, self.y)
+            
+            # tell the knowledge base information gathered from these percepts
+            if current_percept_key != None:            
+                for value in current_percept_key:
+                    if value != '_':
+                        self.ie.tell(value, self.x, self.y)
 
+            # try to infer a plan of action for the next move                
+            # gets move based on answers to the following queries (prioritized via order of questions)
+            # build our choice of move based on inference
+            # note, in the absence of a glimmer, the highest priority moves are those unvisited cells that we believe to be safe, followed by potentially riskier moves (cells not known to be safe)
 
+            choices_Gold = ""
+            choices_Safe = ""
+            choices_perNoStench = ""
+            choices_perNoBreeze = ""
+            choices_perNoObstacle = ""
+            choices_notWumpus = ""
+            choices_notPit = ""
+            choices_notObstacle = ""            
+            choices_Wumpus = ""
+            choices_Pit = ""
+            choices_Obstacle = ""
+            
+            if self.x >= 0 and self.x <= self.size:
+                # explorer can only grab gold when in the same square as the glimmer, and grabbing the gold is the highest priority move
+                choices_Gold = self.ie.ask("GLIMMER({},{})".format(self.x, self.y), self.x, self.y)            
+    
+                # checks what directions are safe, adds them 
+                choices_Safe = self.ie.ask("SAFE({},{})".format(self.x, self.y), self.x, self.y)
+            
+                # if we don't have SAFE(x,y) in KB, try to determine if cell is safe using percept info
+                choices_perNoStench = self.ie.ask("!(STENCH({},{}))".format(self.x, self.y), self.x, self.y)
+            
+                choices_perNoBreeze = self.ie.ask("!(BREEZE({},{}))".format(self.x, self.y), self.x, self.y)                    
+            
+                choices_perNoObstacle = self.ie.ask("!(OBSTACLE({},{}))".format(self.x, self.y), self.x, self.y)   
+            
+                # if we don't know (and can't ascertain) if cell is safe, make checks that would allow a riskier move 
+                choices_notWumpus = self.ie.ask("!(WUMPUS({},{}))".format(self.x, self.y), self.x, self.y) 
+            
+                choices_notPit = self.ie.ask("!(PIT({},{}))".format(self.x, self.y), self.x, self.y) 
+            
+                choices_notObstacle = self.ie.ask("!(OBSTACLE({},{}))".format(self.x, self.y), self.x, self.y)
+              
+                # look at hazards on map
+                choices_Wumpus = self.ie.ask("WUMPUS({},{})".format(self.x, self.y), self.x, self.y) 
+                        
+                choices_Pit = self.ie.ask("PIT({},{})".format(self.x, self.y), self.x, self.y) 
+                       
+                choices_Obstacle = self.ie.ask("OBSTACLE({},{})".format(self.x, self.y), self.x, self.y)              
+                                
+                # add all possible choices together (with no duplicates) to determine the best choice
+                if choices_Gold != "":
+                    for c in choices_Gold:
+                        choices.append(c)
+                if choices_Safe != "":
+                    for c in choices_Safe:
+                        choices.append(c)
+                if choices_perNoStench != "":
+                    for c in choices_perNoStench:
+                        choices.append(c)
+                if choices_perNoBreeze != "":
+                    for c in choices_perNoBreeze:
+                        choices.append(c)
+                if choices_perNoObstacle != "":
+                    for c in choices_perNoObstacle:
+                        choices.append(c)
+                if choices_notWumpus != "":
+                    for c in choices_notWumpus:
+                        choices.append(c)
+                if choices_notPit != "":
+                    for c in choices_notPit:
+                        choices.append(c)
+                if choices_notObstacle != "":
+                    for c in choices_notObstacle:
+                        choices.append(c)
+                if choices_Wumpus != "":
+                    for c in choices_Wumpus:
+                        choices.append(c)
+                if choices_notPit != "":
+                    for c in choices_Pit:
+                        choices.append(c)
+                if choices_notObstacle != "":
+                    for c in choices_Obstacle:
+                        choices.append(c)                
+             
+                for c in choices:
+                    current_valid_choices.add(c)  
+                    
+                choices.clear()
+    
+                if 'Stuck' in current_valid_choices:
+                    stop = True
+                    self.logger.info("Explorer is stuck!")
+                    break            
+                
+                if len(current_valid_choices) > 0:            
+                    #print(list(current_valid_choices)[0])
+                    if list(current_valid_choices)[0] == '<k' or list(current_valid_choices)[0] == '>k' or list(current_valid_choices)[0] == '^k' or list(current_valid_choices)[0] == 'vk':
+                        self.kb.tell("SHOOTARROW({},{})".format(self.x, self.y), self.x, self.y)      
+                        
+                        if list(current_valid_choices)[0] == '<k':
+                            stop, percept = self.make_move(random.choice('<'))
+                            self.move.shoot_wumpus('<')
+                        elif list(current_valid_choices)[0] == '>k':
+                            stop, percept = self.make_move(random.choice('>'))
+                            self.move.shoot_wumpus('>')                            
+                        elif list(current_valid_choices)[0] == 'vk':
+                            stop, percept = self.make_move(random.choice('v'))
+                            self.move.shoot_wumpus('v')
+                        elif list(current_valid_choices)[0] == '^k':
+                            stop, percept = self.make_move(random.choice('^'))
+                            self.move.shoot_wumpus('^')
+                        else:
+                            stop, percept = self.make_move(random.choice(list(current_valid_choices)[0]))                            
+                            current_valid_choices.clear()
+                  
+                # roll the dice, if we are not stuck but it looks like we have no good moves
+                stop, percept = self.make_move(random.choice(list(current_valid_choices)))   
+                current_valid_choices.clear()
 
-            choices = self.ie.ask("What Next?", self.x, self.y)  # Ask Inference Engine for best possible choices
-            if 'Stuck' in choices:
-                stop = True
-                self.logger.info("Explorer is stuck!")
-                break
-            stop, percept = self.make_move(random.choice(choices))
-            if percept:  # new percept
-                self.ie.tell(percept, self.x, self.y)
+                #if percept:  # new percept
+                #    self.ie.tell(percept, self.x, self.y)
+                    
         self.print_stats()
 
     # Make move, return new x, new y, and if gold found
